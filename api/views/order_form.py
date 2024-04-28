506d1e9eb5f5afe3d1b2086b2e9a411d92ace593
Yasenbaka <1260352143@qq.com>
2024-04-28 20:17:22 +0800

    fix：20240428
    修改

--- api/views/order_form.py
+++ api/views/order_form.py
@@ -134,24 +134,138 @@ def delete_order(request):
         }, status=status.HTTP_400_BAD_REQUEST)
     else:
         try:
-            delete_rule = request.POST['delete_rule']
+            will_delete_order_unique_id = int(request.POST['order_unique_id'])
         except KeyError:
             return JsonResponse({
                 'code': 10135,
-                'error': '缺少必要参数！<delete_rule>'
+                'error': '缺少必要参数！<order_unique_id>'
             }, status=status.HTTP_400_BAD_REQUEST)
-        if delete_rule == 'closed_order':
-            try:
-                will_delete_order_unique_id = int(request.POST['order_unique_id'])
-            except KeyError:
-                return JsonResponse({
-                    'code': 10135,
-                    'error': '缺少必要参数！<order_unique_id>'
-                }, status=status.HTTP_400_BAD_REQUEST)
-            openid_order.closed_order['data'] = [item for item in openid_order.closed_order['data'] if item['order_unique_id'] != will_delete_order_unique_id]
+        is_order = None
+        for item in openid_order.closed_order['data']:
+            if item['order_unique_id'] == will_delete_order_unique_id:
+                is_order = True
+                break
+            else:
+                is_order = False
+        if is_order:
+            openid_order.closed_order['data'] = [item for item in openid_order.closed_order['data'] if
+                                                 item['order_unique_id'] != will_delete_order_unique_id]
             openid_order.save()
             return JsonResponse({
                 'code': 10125,
                 'message': '指定订单删除成功！'
             })
+        else:
+            return JsonResponse({
+                'code': 10135,
+                'message': '指定订单删除失败！已结束订单列表中未找到该订单！'
+            })
+
+
+@require_http_methods(['POST'])
+@csrf_exempt
+def change_order_service(request):
+    get_token_re = handle_token(request.headers.get('Authorization'))
+    if not get_token_re['judge']:
+        return JsonResponse({
+            'code': 10112,
+            'error': get_token_re['message']
+        }, status=status.HTTP_403_FORBIDDEN)
+    openid = get_token_re['openid']
+    try:
+        WxUsers.objects.get(openid=openid)
+    except WxUsers.DoesNotExist:
+        return JsonResponse({
+            'code': 10117,
+            'error': '用户未注册'
+        }, status=status.HTTP_403_FORBIDDEN)
+    try:
+        openid_order = OrderForm.objects.get(openid=openid)
+    except OrderForm.DoesNotExist:
+        return JsonResponse({
+            'code': 10135,
+            'error': '用户没有订单！'
+        }, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        order_unique_id = int(request.POST['order_unique_id'])
+    except KeyError:
+        return JsonResponse({
+            'code': 10135,
+            'error': '缺少必要参数！<order_unique_id>'
+        }, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        change_rule = request.POST['change_rule']
+    except KeyError:
+        return JsonResponse({
+            'code': 10135,
+            'error': '缺少必要参数！<change_rule>'
+        })
+    is_order = [None, None]
+    if change_rule == 'ongoing_order':
+        for item in openid_order.ongoing_order['data']:
+            if item['order_unique_id'] == order_unique_id:
+                is_order = [True, 'ongoing_order']
+                break
+            else:
+                is_order = False
+    elif change_rule == 'closed_order':
+        for item in openid_order.closed_order['data']:
+            if item['order_unique_id'] == order_unique_id:
+                is_order = [True, 'closed_order']
+                break
+            else:
+                is_order = False
+    else:
+        return JsonResponse({
+            'code': 10135,
+            'error': '<change_rule>只能接收(ongoing_order),(closed_order)两个参数值！'
+        })
+    if is_order[0]:
+        if is_order[1] == 'ongoing_order':
+            openid_order.service_order['data'].append([item for item in openid_order.ongoing_order['data'] if
+                                                  item['order_unique_id'] == order_unique_id])
+            openid_order.ongoing_order['data'] = [item for item in openid_order.ongoing_order['data'] if
+                                                  item['order_unique_id'] != order_unique_id]
+        elif is_order[1] == 'closed_order':
+            openid_order.service_order['data'].append([item for item in openid_order.closed_order['data'] if
+                                                      item['order_unique_id'] == order_unique_id])
+            openid_order.closed_order['data'] = [item for item in openid_order.closed_order['data'] if
+                                                  item['order_unique_id'] != order_unique_id]
+        else:
+            return JsonResponse({
+                'code': 10135,
+                'error': '服务器出现错误！错误点在：change_order_service-1！'
+            })
+        openid_order.save()
+        return JsonResponse({
+            'code': 10125,
+            'message': f'订单成功删除并登记为售后服务状态！订单号即是售后单号：{order_unique_id}'
+        })
+    else:
+        return JsonResponse({
+            'code': 10135,
+            'error': f'未在指定的规则<{change_rule}>中找到用户的订单！'
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
